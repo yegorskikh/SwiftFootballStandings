e36e68177ed55d160f7ca39b3f77c77fb8af0f6c
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-04-15 12:21:14 +0300

    init: start project

---
+++ SwiftFootballStandings/SwiftFootballStandings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftFootballStandings
+//
+//  Created by Yegor Gorskikh on 15.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+}
+
